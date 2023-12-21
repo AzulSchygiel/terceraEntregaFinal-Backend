c3694796aaee2862937f83bd3e5c15c029305948
AzulSchygiel <132393983+AzulSchygiel@users.noreply.github.com>
2023-12-21 20:09:10 -0300

    Update passport.config.js

--- src/config/passport.config.js
+++ src/config/passport.config.js
@@ -1,92 +1,111 @@
 import passport from "passport";
-import localStrategy from "passport-local";
-import { createHash, invalidPassword } from "../utils.js";
-import { usuariosModel } from "../persistence/mongo/models/usuarios.model.js";
+import localStrategy from "passport-local"; 
+
+// Se creará el hash y la clave se guardará con HASH, más seguro
+import { createHash, inValidPassword } from "../utils.js";
 
 import { config } from "./config.js";
-import GithubStrategy from "passport-github2"; //Estrategia de registro con Github
+import GithubStrategy from "passport-github2"
 
+import { usersDao } from "../dao/index.js";
 
-//Estrategia para registar usuarios: con username y password
-export const initPassport = () => {
+import { logger } from "../helpers/logger.js";
+
+export const initializePassport = () => {
+    // Estrategia para registrar usuarios nuevos
     passport.use("signupLocalStrategy", new localStrategy(
-    {
-        passReqToCallback: true,
-        usernameField: username, //Cuando no hay username se pone "email"
-    },
-    async(req, username, email, password, done) => {
-        const { user } = req.body;
-        try{
-            const usuario = await usuariosModel.findOne({username})
-            if(usuario){
-               return done(null, false); //Usuario registrado
+        {  //Para trabajar con otros datos
+            passReqToCallback: true, //Acceso a objeto request
+            usernameField: "email", //El campo UserName ahora es Email
+        },
+        async (req, username, password, done) => {
+            const {first_name, last_name, age} = req.body;
+            try {
+                // Buscar el email
+                const user = await usersDao.getUserByEmail(username); 
+                if(user){
+                    // Usuario ya registrado
+                    return done(null, false)
+                } else {
+                    const newUser = {
+                        first_name, 
+                        last_name,
+                        age,
+                        email: username, 
+                        password: createHash(password)
+                    };
+                    logger.informativo(newUser);
+                    const userCreated = await usersDao.createUser(newUser)
+                    return done(null, userCreated)
+                }
+            } catch (error) {
+                return done(error)
             }
-            const newUsuario = { //Usuario No registrado
-               username,
-               email,
-               password:createHash(password)
-            };
-        console.log(newUsuario);
-        const usuarioCreado = await usuariosModel.create(newUsuario);
-        return done(null, usuarioCreado);    
-    } catch (error) {
-         return done(error);
-    }
-}
+        }
     ));
-};
 
-//Estrategia para Iniciar Sesión de los usuarios
-//Instalar npm i passport-github2
-passport.use("loginLocalStrategy", new localStrategy(
-{
-        usernameField: "username",
-},
-async(username, password, done) => {
-    try {
-        const usuario = await usuariosModel.findOne({user:username});
-        if(!usuario){
-            return done(null, false); //Usuario No registrado
+    // Estrategia para login de usuarios
+    passport.use("loginLocalStrategy", new localStrategy(
+        { 
+            usernameField: "email", //El campo UserName = Email
+        },
+        async (username, password, done) => {
+            try {
+                const user = await usersDao.getUserByEmail(username)
+                if(!user){
+                    // El usuario no está registrado
+                    return done(null, false)
+                } 
+                if(!inValidPassword(password, user)){
+                    return done(null, false);
+                }
+                //Validamos que el usuario existe y la password es correct
+                return done(null, user);
+            } catch (error) {
+                return done(error);
+            }
         }
-    } catch (error) {
-        return done(error);
-    }
-}
-));
+    ));
 
-//Estrategia para registro con GitHub de sessions.routes
-passport.use("signupGithubStrategy", new GithubStrategy(//Logica para registrar usuarios con Github
-      {//Objeto donde estarán todos los datos de la API de Github
-       clientID: config.github.clientIDGithub,
-       clientSecret: config.github.clientSecretGithub,
-       callbackURL: `http://localhost:8080/api/sessions${config.github.callbackGithub}`
-      },
-    async(accesToken, refreshToken, profile, done) => { //Esos Tokens son generados por la aplicación/red social
-     try { //Verificar si el usuario esta registrado
-        //console.log("profile", profile); //Se mostrarán todos los datos (obtenidos de la cuenta de Github) del usuario registrado
-        const usuario = await usuariosModel.findOne({username:profile.username});
-        if(usuario){
-           return done(null, usuario); //Usuario ya está registrado y se inicia la sesión
+    // Estrategia para registrar con GitHub
+    passport.use("signupGithubStrategy", new GithubStrategy (
+        {
+            clientID: config.github.clientId,
+            clientSecret: config.github.clientSecret,
+            callbackURL: `http://localhost:8080/api/sessions${config.github.callbackUrl}`
+        },
+        async(accessToken, refreshToken, profile, done) => {
+            try {
+                // console.log("profile", profile);
+                const user = await usersModel.findOne({email: profile.username});
+                if(user){ // Si ya está registrado
+                    return done(null, user)
+                } else { // Si no está registrado
+                    const newUser = {
+                        first_name: profile.displayName,
+                        email: profile.username, 
+                        password: createHash(profile.id) // Cualquier dato, por cumplir
+                    };
+                    logger.informativo(newUser);
+                    const userCreated = await usersModel.create(newUser);
+                            // El done (hubo errores?, nuevo user)
+                    return done(null, userCreated)
+                }
+            } catch (error) {
+                return done(error);
+            }
         }
-        const newUsuario = {
-           username:profile.username,
-           email:profile._json.name,
-           password:createHash(profile.id)
-        }; //Usuario No está registrado
-    console.log(newUsuario);
-    const usuarioCreado = await usuariosModel.create(newUsuario); //Se crea el usuario en la Base de Datos
-    return done(null, usuarioCreado); //Se crea a Sesión
-} catch (error) {
-     return done(error);
-}
-}
-));
+    ));
 
-passport.serializeUser((usuario, done) => {
-    done(null, usuario.id);
-});
+    // Serializers
+    passport.serializeUser((user, done) => {
+        done(null, user._id);
+    });
 
-passport.deserializeUser(async(id, done) => {
-    const usuario = await usuariosModel.findById(id);
-    done(null.usuario);
-})
+        //  El SV recibe a cookie, obtiene su valor, extrae la ID del usuario
+        // Busca la ID en la BD, y queda guardado en req.user
+    passport.deserializeUser(async (id, done) => {
+        const user = await usersDao.getUserById(id);
+        done(null, user); //req.user = info del usuario que traemos de BD
+    });
+}
