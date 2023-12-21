479b5cb7ccf81982fdc4c7fe5e5cc823cc2fb0ef
AzulSchygiel <132393983+AzulSchygiel@users.noreply.github.com>
2023-12-21 20:14:12 -0300

    Update carrito.controlador.js

--- src/controller/carrito.controlador.js
+++ src/controller/carrito.controlador.js
@@ -1,100 +1,181 @@
-import { JuegosService } from "../service/videojuegos.servicio.js";
-import { CarritoService } from "../service/carrito.service.js";
-import { v4 as uuidv4 } from 'uuid';
+// Importar Capa de Servicio
+import { CartsService } from "../service/carts.service.js";
+import { ProductsService } from "../service/products.service.js";
+import { TicketsService } from "../service/tickets.service.js"
 
-export class CarritoController{
-    static getcarrito = async(req,res) => {
+import {v4 as uuidv4} from 'uuid';
+
+
+export class CartsController{
+    // Obtener Carritos
+    static getCarts = async (req, res) => {
         try {
-            const carrito = await CarritoService.getCarrito();
-            res.json({status:"success", data:carrito});
+            const carts = await CartsService.getCarts();
+            res.json({data: carts});
         } catch (error) {
             res.json({error:error.message});
         }
     };
 
-    static getcarrito = async(req,res) => {
+    // Obtener Carrito por ID
+    static getCartById = async (req, res) => {
         try {
-            const carritoID = req.params.cid;
-            const carrito = await CarritoService.getCarritoById(carritoID);
-            res.json({status:"success", data:carrito});
+            const cartId = req.params.cid;
+            const cart = await CartsService.getCartById(cartId);
+            res.json({status:"success", data: cart});
         } catch (error) {
             res.json({error:error.message});
         }
     };
 
-    static createcarrito = async(req,res) => {
+    // Crear Carrito
+    static createCart = async (req, res) => {
         try {
-            const carritocreate = await CarritoService.createcarrito();
-            res.json({status:"success", data:carritocreate});
+            const cartCreated = await CartsService.createCart();
+            res.json({status:"success",data: cartCreated, result: "Carrito creado"});
+        } catch (error) {
+            res.json({status:"error",error:error.message});
+        }
+    };
+
+    // Agregar producto en carrito
+    static addProductInCart = async (req, res) => {
+        try {
+            const {cid:cartId, pid:productId} = req.params;
+            const cart = await CartsService.getCartById(cartId); 
+            const product = await ProductsService.getProductById(productId);
+            if((req.user.role === "premium" && product.owner.toString() === req.user._id.toString())){
+                res.json({status:"error",message:"No puedes agregar TUS productos a un carrito"});
+            } else {
+                const result = await CartsService.addProductInCart(cartId, productId);
+                res.json({status:"success", result});
+            }  
         } catch (error) {
             res.json({error:error.message});
         }
     };
 
-    static addProductoToCarrito = async(req,res) => {
+    // Eliminar Producto de un carrito
+    static deteleProduct = async (req, res) => {
         try {
-            const {cid:carritoID, pid:juegoID} = req.params;
-            const {quantity} = req.body
-            const carrito = await CarritoService.getCarritoById(carritoID);
-            const producto = await ProductosService.getProducto(juegoID);
-            const result = await CarritoService.addProducto(carritoID, juegoID);
+            const {cid: cartId, pid: productId} = req.params;
+            const cart = await CartsService.getCartById(cartId);
+            const result = await CartsService.deleteProduct(cartId, productId);
             res.json({status:"success", result});
         } catch (error) {
             res.json({error:error.message});
         }
     };
 
-    static deleteProductoenCarrito = async(req,res) => {
+    // Eliminar un Carrito
+    static deleteCart = async (req, res) => {
         try {
-            const {cid:carritoID, pid:juegoID} = req.params;
-            const carrito = await CarritoService.getCarritoById(carritoID);
-            const result = await CarritoService.deleteProducto(carritoID, juegoID);
-            res.json({status:"success", result});
+            const {cid: cartId} = req.params;
+            const result = await CartsService.deleteCart(cartId);
+            res.json({status:"success", result: "carrito eliminado",});
         } catch (error) {
             res.json({error:error.message});
         }
     };
 
-    static updateProductoenCarrito = async(req,res) => {
+    // Actualizar Cantidad de Productos en el Carrito
+    static updateProductInCart = async (req, res) => {
         try {
-            const {cid:carritoID, pid:juegoID} = req.params;
+            const {cid: cartId, pid: productId} = req.params;
             const {newQuantity} = req.body;
-            const carrito = await CarritoService.getCarritoById(carritoID);
-            const result = await CarritoService.updateProductoenCarrito(carritoID, juegoID);
+            const cart = await CartsService.getCartById(cartId);
+            const result = await CartsService.updateProductCart(cartId, productId, newQuantity);
             res.json({status:"success", result});
         } catch (error) {
             res.json({error:error.message});
         }
     };
 
-    static purchaseCarrito = async(req,res) => {
+    // Crear Ticket de Purchase
+    static purchaseCart = async (req, res) => {
         try {
-            const {cid:carritoID} = req.params;
-            const carrito = await CarritoService.getCarritoById(carritoID);
-            if(carrito.producto.length){
-                const ticketProducto = [];
-                const rejectProducto = [];
-                for(let i=0; i<carrito.producto.length; i++){
-                    const productoenCarrito = carrito.producto[i];
-                    const productoInfo = productoenCarrito.juegoID;
-            if(productoenCarrito.quantity < productoInfo.stock){
-            ticketProducto.push(productoenCarrito);
-            }else{
-                rejectProducto.push(productoenCarrito);
-            }
-            };
-            const newTicket = {
-                code: uuidv4(),
-                purchase_datetime: new Date(),
-                amount: 100,
-                purchaser: req.usuario.email
-            };
-            res.json({status:"success", message:"Compra realizada", rejectProducto});
-            }else{
-                res.json({status:"error", message:"El carrito está vacio"});
+            const {cid: cartId} = req.params;
+            const cart = await CartsService.getCartById(cartId);
+            // Verificar Stock de Cada Producto
+            if(cart.products.length){
+                const ticketProducts = [];
+                const rejectedProducts = [];
+                for (let i = 0; i < cart.products.length; i++) {
+                    // Dato de todo el producto
+                    const cartProduct = cart.products[i];
+                    const productInfo = cartProduct.productId;
+                    // Quantity VS Stock
+                    if (cartProduct.quantity <= productInfo.stock) {
+                        ticketProducts.push(cartProduct);
+                        productInfo.stock -= cartProduct.quantity;
+                    } else {
+                        rejectedProducts.push(cartProduct);
+                    }
+                };
+                console.log("ticketProducts", ticketProducts);
+                console.log("rejectedProducts", rejectedProducts);
+
+                let total = 0;
+                for (let i = 0; i < ticketProducts.length; i++) {
+                    total += ticketProducts[i].productId.price * ticketProducts[i].quantity;
+                };
+
+                // Crear el ticket en BD
+                if(ticketProducts.length >= 1){
+                    const newTicket = {
+                        code: uuidv4(),
+                        purchaseDateTime: new Date(),
+                        amount: total,
+                        purcharser: req.user.email //El comprador, el usuario
+                    };
+
+                    const ticket = await TicketsService.createTicket(newTicket);
+                    console.log("New Ticket", ticket);
+                };
+
+                // Actualizar el carrito con productos rechazados
+                if(rejectedProducts.length >= 1 && ticketProducts.length >= 1){
+                    for (let i = 0; i < ticketProducts.length; i++) {
+                        // Dato de todo el producto
+                        let sellProduct = ticketProducts[i];
+                        let productId = sellProduct.productId._id;
+                        // Control de Stock
+                        let stock = sellProduct.productId.stock;
+                        // stock -= sellProduct.quantity;
+                        console.log("carrito",cartId);
+                        console.log("id",productId);
+                        await ProductsService.updatedProduct(productId, stock);
+    
+                        // Eliminar estos productos del carrito
+                        await CartsService.deleteProduct(cartId, productId)
+                    };
+
+                    res.json({status: "success", message: "Compra Realizada, algunos productos no se pudieron comprar por falta de stock:", rejectedProducts})
+                } else if(rejectedProducts.length >= 1 && ticketProducts.length == 0){
+                    res.json({status: "error", message: "No es posible concretar la venta, algunos productos tienen falta de stock:", rejectedProducts})
+                } else {
+                    for (let i = 0; i < ticketProducts.length; i++) {
+                        // Dato de todo el producto
+                        let sellProduct = ticketProducts[i];
+                        let productId = sellProduct.productId._id;
+                        // Control de Stock
+                        let stock = sellProduct.productId.stock;
+                        // stock -= sellProduct.quantity;
+                        await ProductsService.updatedProduct(productId, stock);
+    
+                        // Eliminar estos productos del carrito
+                        await CartsService.deleteProduct(cartId, productId)
+                    };
+                    console.log(`Carrito con ID ${cartId} vacio, ya que se vendieron todos los productos. \nIngresa nuevos productos`);
+                    res.json({status: "success", message: "Compra Realizada", ticketProducts})
+                };
+            } else {
+                res.json({status: "error", message: "El carrito está vacio"})
             }
         } catch (error) {
             res.json({error:error.message});
         }
     };
+
+};
 }
